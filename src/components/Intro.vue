95b96b631c37ed7957f3ab2645a9ad89a12581b6
david-gi <davidgi@gmx.com>
2019-05-29 12:53:21 -0400

    Intro video change

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,11 +1,11 @@
 <template>
 		<div v-show="isOpen" id="introModal" @keypress="closeIntro" class="rounded-top border border-primary"
-			style="width:25%; height:30%; position:fixed; bottom:0; right:0; border-width:3px !important;">
+			style="width:228px; height:159px; position:fixed; bottom:0; right:0; border-width:3px !important;">
 						<button type="button" class="mb-n4 close text-light" @click="closeIntro">
 							<span aria-hidden="true">&times;</span>
 						</button>
 			<iframe class="w-100 h-100" width="auto" height="auto" title="How to use Glutenous!" 
-				src="https://www.youtube.com/embed/8UDToGtQYIQ?controls=1&amp;start=1" frameborder="0" 
+				src="https://www.youtube.com/embed/vf15MhiT-OU?controls=1&amp;start=1" frameborder="0" 
 				allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>
 			<br>
 		</div>
